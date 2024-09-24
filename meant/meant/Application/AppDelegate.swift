4b863d7dd1a55a6b9f41cbdca64aa90b82e1c134
noeyiz <goojiong@gmail.com>
2024-09-24 20:02:50 +0900

    [Feat] 초기 프로젝트 설정 (#2)
    * docs: gitignore 파일 추가
    
    * feat: 프로젝트 생성
    
    * chore: 프로젝트 설정 업데이트
    - 지원 대상을 iPhone으로만 제한
    - 최소 배포 대상을 iOS 17.0으로 상향 조정
    - 화면 방향을 세로 모드로 고정
    
    * feat: 스토리보드 제거
    
    * refactor: 파일 구조 업데이트
    - Application, Presentation, Domain, Data, Common의 구조

---
+++ meant/meant/Application/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  meant
+//
+//  Created by 지연 on 9/24/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
+    }
+
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {}
+}
+
