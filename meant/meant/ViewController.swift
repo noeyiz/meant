6dd7b78912f716fb70b1ac8264d93e5397f9ccb9
noeyiz <lamoureclatant@gmail.com>
2024-09-24 19:34:32 +0900

    feat: 프로젝트 생성

---
+++ meant/meant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  meant
+//
+//  Created by 지연 on 9/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
