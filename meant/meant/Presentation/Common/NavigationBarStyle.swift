89d7a5a178acf1260c06a6abd385d32c2f91c4b6
noeyiz <lamoureclatant@gmail.com>
2024-09-25 01:20:36 +0900

    feat: NavigationBarStyle 및 NavigationBarProtocol 정의

---
+++ meant/meant/Presentation/Common/NavigationBarStyle.swift
@@ -0,0 +1,13 @@
+//
+//  NavigationBarStyle.swift
+//  meant
+//
+//  Created by 지연 on 9/24/24.
+//
+
+import Foundation
+
+enum NavigationBarStyle {
+    case largeTitleWithRightButton
+    case normalTitleWithBothButtons
+}
